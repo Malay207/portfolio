39503fcacfe9bac96364267a2cf3462d858b636a
malaykumar <malaydev207@gmail.com>
2025-01-18 17:13:47 +0530

    update

--- src/app/sendmsg.js
+++ src/app/sendmsg.js
@@ -1,10 +1,10 @@
 "use client"
 import { useState } from "react"
-import { ToastContainer,toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
-  import { API_URL } from "@/config/constant";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { API_URL } from "@/config/constant";
 export function SendMsg() {
-    const notify = () => toast("Thank You!");
+    const notify = (data) => toast(data);
     const [data, setdata] = useState({
         name: "",
         email: "",
@@ -20,9 +20,9 @@ export function SendMsg() {
         })
     }
     const handlesubmit = async () => {
-        
 
-        if(!API_URL){
+
+        if (!API_URL) {
             return null;
         }
         const res = await fetch(`${API_URL}/api/sendmail`, {
@@ -48,20 +48,26 @@ export function SendMsg() {
             <form>
                 <input type="text" name="name" value={data.name} onChange={handlechange} placeholder="Name" className="border-2 border-gray-200 rounded-3xl p-3 w-full my-7 text-black" required minLength={3} />
 
-                <input type="email" name="email" value={data.email} onChange={handlechange} placeholder="Email" className="border-2 border-gray-200 rounded-3xl p-3 w-full my-7 text-black" required/>
+                <input type="email" name="email" value={data.email} onChange={handlechange} placeholder="Email" className="border-2 border-gray-200 rounded-3xl p-3 w-full my-7 text-black" required />
 
                 <textarea placeholder="Message" name="message" value={data.message} onChange={handlechange} className="border-2 border-gray-200 rounded-3xl p-3 w-full my-7 text-black" cols="5" rows="5" required minLength={5}></textarea>
 
-                <button className="text-xl border-2 border-white flex items-center  rounded-3xl px-8 py-1.5 hover:bg-white hover:text-black " onClick={(e) => {
+                <button className="text-xl border-2 border-white flex items-center  rounded-3xl px-8 py-1.5 hover:bg-white hover:text-black " onClick={async (e) => {
                     console.log("hello");
                     e.preventDefault();
-                    handlesubmit();
-                    notify();
+                    const data = await handlesubmit();
+                    if (data.sucess) {
+                        notify("Thank You!");
+                    }
+                    else{
+                        notify("Failed to send mail.Try again Later!");
+                    }
+
                 }} disabled={data.name === "" || data.email === "" || data.message === "" ? true : false}>
 
                     <span className="">submit</span>
                 </button>
-                <ToastContainer/>
+                <ToastContainer />
             </form>
         </>
     )
